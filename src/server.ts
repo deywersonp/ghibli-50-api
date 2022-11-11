f68b153a0705d82ed7eb38fb05f67857fddf17ef
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-11-11 18:08:36 -0300

    created api documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from "swagger-ui-express";
+
 import { routes } from './routes';
+import swaggerFile from "./swagger.json";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
