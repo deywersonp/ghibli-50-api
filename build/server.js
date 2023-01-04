e24b87d8482d9b6b0e576eea11b58676ad67e5a9
Deywerson Pereira <deywerson.pereira@gmail.com>
2023-01-03 22:19:42 -0300

    fix: swagger-ui.css path

--- build/server.js
+++ build/server.js
@@ -5,19 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
-const path_1 = __importDefault(require("path"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const routes_1 = require("./routes");
 const swagger_json_1 = __importDefault(require("./swagger.json"));
 const app = (0, express_1.default)();
-const ROOT_FOLDER = path_1.default.join(__dirname, '..');
-const SRC_FOLDER = path_1.default.join(ROOT_FOLDER, 'src');
-const options = { customCssUrl: '/public/swagger-ui.css', customSiteTitle: "Ghibli.50 API - Swagger Documentation" };
+const options = { customCssUrl: '/public/css/swagger-ui.css', customSiteTitle: "Ghibli.50 API - Swagger Documentation" };
 app.use((0, cors_1.default)({
     exposedHeaders: 'x-total-count'
 }));
 app.use(express_1.default.json());
-app.use('/public', express_1.default.static(path_1.default.join(SRC_FOLDER, 'public')));
+app.use('/public/css', express_1.default.static('public/css'));
 app.use("/", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default, options));
 app.use(routes_1.routes);
 app.listen(process.env.PORT || 3333, () => {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import cors from 'cors';
-import path from 'path';
 import swaggerUi from "swagger-ui-express";
 
 import { routes } from './routes';
@@ -8,16 +7,14 @@ import swaggerFile from "./swagger.json";
 
 const app = express();
 
-const ROOT_FOLDER = path.join(__dirname, '..');
-const SRC_FOLDER = path.join(ROOT_FOLDER, 'src');
-const options = { customCssUrl: '/public/swagger-ui.css', customSiteTitle: "Ghibli.50 API - Swagger Documentation" };
+const options = { customCssUrl: '/public/css/swagger-ui.css', customSiteTitle: "Ghibli.50 API - Swagger Documentation" };
 
 app.use(cors({
   exposedHeaders: 'x-total-count'
 }));
 
 app.use(express.json());
-app.use('/public', express.static(path.join(SRC_FOLDER, 'public')));
+app.use('/public/css', express.static('public/css'));
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
 app.use(routes);
 
