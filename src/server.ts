76ac4eff72dce1ae613b7fd8dc48062b5a5efbc6
Deywerson Pereira <deywerson.pereira@gmail.com>
2023-01-03 22:00:36 -0300

    fix: add aditional config to use styles at swagger docs

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
 import swaggerUi from "swagger-ui-express";
 
 import { routes } from './routes';
@@ -7,13 +8,17 @@ import swaggerFile from "./swagger.json";
 
 const app = express();
 
+const ROOT_FOLDER = path.join(__dirname, '..');
+const SRC_FOLDER = path.join(ROOT_FOLDER, 'src');
+const options = { customCssUrl: '/public/swagger-ui.css', customSiteTitle: "The Words That I Know API - Swagger" };
+
 app.use(cors({
   exposedHeaders: 'x-total-count'
 }));
 
 app.use(express.json());
-
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/public', express.static(path.join(SRC_FOLDER, 'public')));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile, options));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
