f26688151727607869ec737ca60bc6f703a70dce
Deywerson Pereira <deywerson.pereira@gmail.com>
2023-01-03 22:02:43 -0300

    add: build after new swagger config

--- build/server.js
+++ build/server.js
@@ -5,15 +5,20 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const path_1 = __importDefault(require("path"));
 const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const routes_1 = require("./routes");
 const swagger_json_1 = __importDefault(require("./swagger.json"));
 const app = (0, express_1.default)();
+const ROOT_FOLDER = path_1.default.join(__dirname, '..');
+const SRC_FOLDER = path_1.default.join(ROOT_FOLDER, 'src');
+const options = { customCssUrl: '/public/swagger-ui.css', customSiteTitle: "The Words That I Know API - Swagger" };
 app.use((0, cors_1.default)({
     exposedHeaders: 'x-total-count'
 }));
 app.use(express_1.default.json());
-app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
+app.use('/public', express_1.default.static(path_1.default.join(SRC_FOLDER, 'public')));
+app.use("/", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default, options));
 app.use(routes_1.routes);
 app.listen(process.env.PORT || 3333, () => {
     console.log('HTTP Server Running!');
